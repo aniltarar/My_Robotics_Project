69422be0aa57dbb5709337e12aa513b49ff4b7d0
Anil <42482653+aniltarar@users.noreply.github.com>
2020-06-16 18:54:14 +1000

    Create screenmodule.py

---
+++ GUI/screenmodule.py
@@ -0,0 +1,17 @@
+# screenmodule.py
+
+import grovepi
+from grove_rgb_lcd import *
+
+import grovepi
+import math
+# Connect the Grove Temperature & Humidity Sensor Pro to digital port D4
+# This example uses the blue colored sensor.
+# SIG,NC,VCC,GND
+sensor = 6  # The Sensor goes on digital port 4.
+
+# temp_humidity_sensor_type
+# Grove Base Kit comes with the blue sensor.
+blue = 0    # The Blue colored sensor.
+white = 1   # The White colored sensor.
+[temp,humidity] = grovepi.dht(sensor,blue) 
